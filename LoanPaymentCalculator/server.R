427d982fef8d66ee8c9206d71540124f7366328e
notbadhuh <bbskater@gmail.com>
2014-11-24 07:16:12 -0500

    First commit

---
+++ LoanPaymentCalculator/server.R
@@ -0,0 +1,32 @@
+shinyServer(function(input, output) {
+     library(ggplot2)
+     output$payment <- renderText({
+          #input$calculate
+          payment <- format(round((input$r/(12*100)*input$P)/(1-(1+input$r/(12*100))^(-input$n*12)),2), nsmall=2)
+          paste("Your monthly payments will be $", payment, ".", sep="")
+     })
+     output$totalInterestPaid <- renderText({
+          payment <- round((input$r/(12*100)*input$P)/(1-(1+input$r/(12*100))^(-input$n*12)),2)
+          totalInterestPaid <- format(round(payment*(input$n*12)-input$P,2), nsmall=2)
+          paste("If you make the normal payments (i.e. you pay no extra), the total interest paid on the loan will be $", totalInterestPaid, ".", sep="")
+     })
+     output$main_plot <- renderPlot({
+          
+          extra <- seq(from=0, to=input$P*0.02, by=input$P*0.001)
+          timeToPayOffLoan <- numeric()
+          payment <- (input$r/(12*100)*input$P)/(1-(1+input$r/(12*100))^(-input$n*12))
+          for(i in 1:length(extra)) {
+               newPayment <- payment + extra[i]
+               timeToPayOffLoan[i] <- (-log(1-(input$r/(12*100)*input$P)/(newPayment))/log(1+input$r/(12*100)))/12
+          }
+          
+          qplot(extra, timeToPayOffLoan, 
+                geom=c("point","line"),
+                main="Time to Pay Off Loan When Paying Extra", 
+                xlab="Extra Paid in Addition to Monthly Payment ($)",
+                ylab="Time to Pay Off Loans (Years)",
+                ylim=c(0,input$n))
+     })
+     
+     
+})
---
+++ LoanPaymentCalculator/ui.R
@@ -0,0 +1,38 @@
+shinyUI(pageWithSidebar(
+     headerPanel(h1("Monthly Loan Payments",align="center"),head('Monthly Loan Payments')),
+     sidebarPanel(
+          h3('Loan Information'),
+          numericInput(inputId = "P",
+                      label = "Loan Principle",
+                      value=60000),          
+          numericInput(inputId = "r",
+                       label = "Loan APR (%)",
+                       value=6.5,
+                       min=0,
+                       max=25,
+                       step=0.1),   
+          numericInput(inputId = "n",
+                       label = "Loan Length (Years)",
+                       value=10,
+                       min=0,
+                       max=25,
+                       step=1),
+          submitButton("Calculate"),
+          br(),
+          br(),
+          h3('How To Use This App'),
+          p("To use this app, just enter your loan information.  This include the loan principle (the amount you've borrowed), the interest rate given as the APR, and the length of the loan in years (how long you are borrowing for).  Then hit calculate.  It's as simple as that!"),
+          br(),
+          a('Github Repository',href='https://github.com/notbadhuh/DevelopingDataProducts')
+     ),
+     mainPanel(
+          textOutput('payment'),
+          br(),
+          textOutput('totalInterestPaid'),
+          br(),
+          p('The following plot shows the time to pay off the loan if you pay an extra amount (in addition to the calculated payment) towards your loan each month.'),
+          plotOutput(outputId = "main_plot", height = "400px"),
+          p('The takeaway is that by paying a little extra each month, you can significantly decrease the time it takes to pay off your loan.  This costs you less in interest over time!'),
+          p('Note: This assumes there is no', a('prepayment penalty',href="http://www.thetruthaboutmortgage.com/prepayment-penalty-mortgage/"),'to worry about.')
+     )
+))
